c3e9de3a58994ba4b9a332d16c5a0bd14d3fb943
Sameeksha Patel <akankshapatellko128@gmail.com>
2026-04-03 04:48:25 +0530

    Add files via upload

---
+++ peakElement.java
@@ -0,0 +1,18 @@
+public class peakElement {
+  
+  public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        int start = 1;
+        int end = nums.length-2;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid]>nums[mid-1]) start = mid+1; // going up 
+            else end = mid-1;
+        }
+        return -1 ;
+    }
+}
