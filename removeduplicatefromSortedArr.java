c19ce3ea2ba268c18eae454fb43ad6178b5cc384
Sameeksha Patel <akankshapatellko128@gmail.com>
2026-04-14 22:09:42 +0530

    Add files via upload

---
+++ removeduplicatefromSortedArr.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class removeduplicatefromSortedArr {
+  
+  public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]!=nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j+1;
+  }
+}
