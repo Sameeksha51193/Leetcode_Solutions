fe438161516be0e83680269ca785a997df7e900b
Sameeksha Patel <akankshapatellko128@gmail.com>
2026-04-15 23:08:17 +0530

    Add files via upload

---
+++ linearSearch.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class linearSearch {
+  
+  public int linearSearch(int nums[], int target) {
+        int n = nums.length;
+		for(int i = 0;i<n;i++){
+            if(nums[i]==target) return i;
+        }
+        return -1;
+    }
+}
