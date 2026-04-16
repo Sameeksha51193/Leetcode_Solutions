8ba0d06eb4ae5e36004c6be517560acb3e7ab066
Sameeksha Patel <akankshapatellko128@gmail.com>
2026-04-16 22:01:00 +0530

    Add files via upload

--- maxConsecutiveOnes.java
+++ maxConsecutiveOnes.java
@@ -1,55 +1,19 @@
-package TwoPointers&SlidingWindow;
+package Arrays;
 
 public class maxConsecutiveOnes {
   
-  // OPTIMAL
-    public int longestOnes(int[] nums, int k) {
-        int maxLen = 0;
-        int l = 0;
-        int r = 0;
-        int countZeroes = 0;
-        while(r<nums.length){
-            if(nums[r]==0) countZeroes++;
-            if(countZeroes>k){ // invalid window so shrink
-                if(nums[l]==0) countZeroes--;
-                l++;
-            }
-            if(countZeroes<=k)maxLen = Math.max(maxLen,r-l+1); // valid window
-            r++;
+  public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int maxCount = 0;
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+                maxCount = Math.max(maxCount,count);
+            } 
+            else count = 0;
         }
-        return maxLen;
-    }
-
-    /* BETTER - O(2N)
-    public int longestOnes(int[] nums, int k) {
-        int maxLen = 0;
-        int l = 0;
-        int r = 0;
-        int countZeroes = 0;
-        while(r<nums.length){
-            if(nums[r]==0) countZeroes++;
-            while(countZeroes>k){ // invalid window so shrink
-                if(nums[l]==0) countZeroes--;
-                l++;
-            }
-            if(countZeroes<=k) maxLen = Math.max(maxLen,r-l+1); // valid window
-            r++;
-        }
-        return maxLen;
-    } */
-
-    /* BRUTE
-    public int longestOnes(int[] nums, int k) {
-        int maxLen = 0;
-        for(int i = 0;i<nums.length;i++){
-            int countZeroes = 0;
-            for(int j = i;j<nums.length;j++){
-                if(nums[j]==0) countZeroes++;
-                if(countZeroes>k) break; 
-                int len = j-i+1;
-                maxLen = Math.max(len,maxLen);
-            }
-        }
-        return maxLen;
-    } */
+        return maxCount;
+  }
+  
 }
